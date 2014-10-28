f555da9e9b106d686731ce39e66a7083ed6ca197
Jason Whitlark <jason@whitlark.org>
2014-10-28 11:29:38 -0700

    Initial project generation.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.bittorrent.scribble "0.1.0-SNAPSHOT"
+  :description "Clojure library for generating Graphite graph URLs."
+  :url "https://github.com/bittorrent/org.bittorrent.scribble"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ test/org/bittorrent/scribble_test.clj
@@ -0,0 +1,7 @@
+(ns org.bittorrent.scribble-test
+  (:require [clojure.test :refer :all]
+            [org.bittorrent.scribble :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
