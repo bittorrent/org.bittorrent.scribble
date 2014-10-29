dd1be05224b4c7a5543ef1bd7b134b86f156bb37
Aaron Cohen <acohen@bittorrent.com>
2014-10-29 12:23:20 -0700

    Make this 0.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject org.bittorrent.scribble "0.1.0-SNAPSHOT"
+(defproject org.bittorrent.scribble "0.1.0"
   :description "Clojure library for generating Graphite graph URLs."
   :url "https://github.com/bittorrent/org.bittorrent.scribble"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :plugins [[com.jakemccrary/lein-test-refresh "0.5.2"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
