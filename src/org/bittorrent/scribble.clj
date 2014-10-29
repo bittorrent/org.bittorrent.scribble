ae31420df8b45ab8f0a115086ba7b7eca3f4bc7c
Aaron Cohen <acohen@bittorrent.com>
2014-10-29 10:18:23 -0700

    Add build-url

--- src/org/bittorrent/scribble.clj
+++ src/org/bittorrent/scribble.clj
@@ -1,8 +1,12 @@
 (ns org.bittorrent.scribble
-  (:import (clojure.lang PersistentVector))
+  (:import (clojure.lang PersistentVector)
+           (java.net URLEncoder))
   (:require [clojure.string :as str]))
 
 (defn build-host [host-map]
+  "Builds host portion of URL"
+  (if (not (contains? host-map :host))
+    (throw (Exception. "No host specified.")))
   (if (and (get host-map :passwd)
            (not (get host-map :user)))
     (throw (Exception. "Passwd field without user field.")))
@@ -19,15 +23,25 @@
        "/render"))
 
 (defn param-val-to-vector [val]
+  "Wraps value in vector if not a vector"
   (if (vector? val)
     val
     [val]))
 
 (defn build-params-for-key [pair]
-  (map #(str (name (key pair)) "=" %) (param-val-to-vector (val pair))))
+  "Generates vector full of 'key=value' strings"
+  (map #(str (name (key pair)) "=" (URLEncoder/encode (str %))) (param-val-to-vector (val pair))))
 
 (defn build-params [params]
+  "Builds entire parameters section of URL"
   (if (not (empty? params))
     (str "?" (str/join "&" (flatten (map build-params-for-key params))))
     ""))
 
+(defn build-url [host-params graph-params]
+  "Combines host params and graph/target params into a URL that will return a graph image.
+  Requires two dicts, host-params, and graph-params.
+
+  host-params keys: :host (required), :scheme (defaults to http), :user (optional), :passwd (optional), :port (optional)
+  graph-params keys: anything supported by graphite. Values can be a vector to repeat multiple targets."
+  (str (build-host host-params) (build-params graph-params)))
--- test/org/bittorrent/scribble_test.clj
+++ test/org/bittorrent/scribble_test.clj
@@ -22,9 +22,9 @@
     (is (= (build-host host-map-2)
            "https://user:passwd@graphite.bittorrent.com:1234/render"))
     (is (thrown? Exception (build-host bad-map-1)))
+    (is (thrown? Exception (build-host {})))
     ))
 
-;;from=-2hours&height=20&until=now&width=120&hideGrid=true&hideLegend=true&hideAxes=true&bgcolor=white&fgcolor=black&margin=0&colorList=black
 
 (def single-param {:from "-2hours"})
 
@@ -36,12 +36,18 @@
 (def repeated-params (array-map :from "-2hours"
                                 :target ["some.stats.one" "some.stats.two"]))
 
+(def encodable-params (array-map :from "-2 hours"
+                                 :target ["hitcount(some.stats.one, '1 min')" "hitcount(some.stats.two, '3 hours')"]))
+
 (deftest build-params-for-key-test
   (testing ""
     (is (= (build-params-for-key (first {:target (:target repeated-params)}))
-           ["target=some.stats.one" "target=some.stats.two"]))))
+           ["target=some.stats.one" "target=some.stats.two"]))
+    (is (= (build-params-for-key (first {:target (:target encodable-params)}))
+           ["target=hitcount%28some.stats.one%2C+%271+min%27%29"
+            "target=hitcount%28some.stats.two%2C+%273+hours%27%29"]))))
 
-(deftest construct-params-test
+(deftest build-params-test
   (testing ""
     (is (= (build-params {})
            ""))
@@ -52,3 +58,8 @@
     (is (= (build-params repeated-params)
            "?from=-2hours&target=some.stats.one&target=some.stats.two"))
     ))
+
+(deftest build-url-test
+  (testing ""
+    (is (= (build-url host-map-1 repeated-params)
+           "http://graphite.bittorrent.com/render?from=-2hours&target=some.stats.one&target=some.stats.two"))))
